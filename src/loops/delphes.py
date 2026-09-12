c749b37577142f48d649dc574f8dd1c5ea9b8f46
A.M.M. Elsayed <ahmedphysica@outlook.com>
2026-09-12 13:36:54 +0800

    Remove unnecessary newline print statement in load_delphes function

--- src/loops/delphes.py
+++ src/loops/delphes.py
@@ -11,7 +11,6 @@ def load_delphes():
     ROOT.gInterpreter.Declare('#include "classes/SortableObject.h"')
     ROOT.gInterpreter.Declare('#include "external/ExRootAnalysis/ExRootTreeReader.h"')
     ROOT.gROOT.SetBatch(True)
-    print("\n")
     print("Using ROOT version:", ROOT.__version__)
     print("Using Delphes libraries found at:", DELPHES_PATH)
     print("\n")
