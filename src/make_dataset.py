3024f3e06222cd7acab037b4d9a86fef37461709
A.M.M. Elsayed <ahmedphysica@outlook.com>
2026-09-18 21:13:55 +0800

    Update merge_method parameter in make_dataset function

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -93,7 +93,7 @@ def make_dataset(
                     loop_tree,
                     max_workers=max_workers,
                     n_chunks=n_chunks,
-                    merge_method=1,
+                    merge_method=2,
                     inputRootFile=str(sampleRootFile),
                     treeName=proc_treeName,               # <-- same tree name for every sample
                     eventWeight=proc_eventWeight,
